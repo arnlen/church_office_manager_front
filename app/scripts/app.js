d65434a6486f1ccaee247acf8765aa92bb653f39
Arnaud Lenglet <arlenglet@gmail.com>
2014-09-01 17:03:26 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name churchOfficeManagerFrontApp
+ * @description
+ * # churchOfficeManagerFrontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('churchOfficeManagerFrontApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name churchOfficeManagerFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the churchOfficeManagerFrontApp
+ */
+angular.module('churchOfficeManagerFrontApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
