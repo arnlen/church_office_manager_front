3759db98acba1e52712f1daf287164571b18e1f8
Arnaud Lenglet <arlenglet@gmail.com>
2014-09-04 19:59:49 +0200

    feat(services): add first panel version + improve switch effect

--- app/scripts/controllers/offices.js
+++ app/scripts/controllers/offices.js
@@ -4,6 +4,11 @@ app.controller('OfficesController', function ($scope, officeFactory) {
 
 	officeFactory.get().$promise.then(function(result) {
 		$scope.office = result.offices[0];
+		$scope.selectedService = $scope.office.services[0];
 	});
+
+	$scope.loadClickedService = function(clicked_service) {
+		$scope.selectedService = clicked_service;
+	}
 
 });
