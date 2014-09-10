77f76e101f32674ba472484efb136f589c5019a7
Arnaud Lenglet <arlenglet@gmail.com>
2014-09-10 18:55:16 +0200

    chore(bower): update Bower packages + add ngAnimate

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,4 +8,4 @@
  *
  * Main module of the application.
  */
-var app = angular.module('churchOfficeManagerFrontApp', ['ngResource']);
+var app = angular.module('churchOfficeManagerFrontApp', ['ngResource', 'ngAnimate']);
