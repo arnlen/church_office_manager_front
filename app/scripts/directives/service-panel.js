7412afaec8ce749a724cb9619de7589d3ab26250
Arnaud Lenglet <arlenglet@gmail.com>
2014-09-21 23:08:44 +0200

    refactor(service panel): try communciation between view <-> directive... without success FTM (Caution! Unstable state! :fire:)

--- app/scripts/directives/service-panel.js
+++ app/scripts/directives/service-panel.js
@@ -1,15 +1,32 @@
 'use strict';
 
-app.directive('chServicePanel', function () {
+app.directive('chServicePanel', ['servicesService', function (servicesService) {
 
 	function link(scope, element, attr) {
 	}
 
+	function controller($scope, servicesService) {
+
+		$scope.servicePanelOpen = false;
+
+		$scope.$watch('selectedService', function(service) {
+			console.log($scope.selectedService);
+		});
+
+		$scope.toggleServicePanel = function() {
+			servicePanelOpen = !servicePanelOpen;
+			console.log('Hit!');
+		};
+	}
+
 	return {
 		restrict: 'E',
+		scope: {
+			selectedService: '='
+		},
+		templateUrl: 'views/services/show.html',
 		link: link,
-		// template: '<div>MyDirective</div>'
-		templateUrl: 'views/services/show.html'
+		controller: controller
 	};
 
-});
+}]);
